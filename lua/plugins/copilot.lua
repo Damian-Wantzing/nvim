c51a91628a2c059f4e081cdef13bacffedf40a8d
Damian Wantzing <damian.wantzing@copernica.com>
2024-06-19 18:07:03 +0200

    added copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,11 @@
 return {
-  { 'github/copilot.vim' },
+  'github/copilot.vim',
+  config = function()
+    -- set keymap for disabling and enabling copilot
+    vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<CR>')
+    vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<CR>')
+
+    -- set keymap for checking status of copilot
+    vim.keymap.set('n', '<leader>cs', '<cmd>Copilot status<CR>')
+  end,
 }
