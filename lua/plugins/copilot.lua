a9228b0cf06ce4ae15bda9d123ce7581acf22b60
Damian Wantzing <damian.wantzing@copernica.com>
2025-06-18 10:47:56 +0200

    enable/disable globally

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,10 +1,29 @@
+local state = require 'copilotstate'
+
+vim.keymap.set('n', '<leader>ce', function()
+  state.store(true)
+end)
+vim.keymap.set('n', '<leader>cd', function()
+  state.store(false)
+end)
+
+local function reload()
+  local on = state.load()
+
+  if on then
+    vim.cmd 'Copilot enable'
+  else
+    vim.cmd 'Copilot disable'
+  end
+end
+
+state.watch(function()
+  reload()
+end)
+
 return {
   'github/copilot.vim',
   config = function()
-    -- set keymap for disabling and enabling copilot
-    vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<CR>')
-    vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<CR>')
-
     -- set keymap for checking status of copilot
     vim.keymap.set('n', '<leader>cs', '<cmd>Copilot status<CR>')
   end,
